1f2c4a80eff1fecf99fca759b3b882d49402eb7a
Ameerul Basheer <ameerulbasheer@outlook.com>
2022-07-08 16:26:32 +0800

    Commit: 002 - TextEditor for Team input and Match input - Placeholder text set to required syntax - Alert for help message for required syntax - scaffold for navigation link

--- WeAreTheChampions/ContentView.swift
+++ WeAreTheChampions/ContentView.swift
@@ -8,9 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showTeamsInputHelp = false
+    @State private var teamInputText = ""
+    @State private var showMatchInputHelp = false
+    @State private var matchInputText = ""
     var body: some View {
-        Text("We are the champions!")
-            .padding()
+        NavigationView {
+            List {
+                Section(content: {
+                    ZStack(alignment: .leading) {
+                        if teamInputText.isEmpty {
+                            HStack {
+                                Text("<Team A name> <Team A registration date in DD/MM> <Team A group number>")
+                                    .font(.caption)
+                                    .foregroundColor(.secondary)
+                                Spacer()
+                            }
+                        }
+                        TextEditor(text: $teamInputText)
+                            .font(.caption)
+                            .foregroundColor(.primary)
+                    }
+                    
+                }, header: {
+                    HStack {
+                        Text("Input your teams here")
+                        Button {
+                            showTeamsInputHelp = true
+                        } label: {
+                            Image(systemName: "info.circle")
+                        }
+                    }
+                    .alert(isPresented: $showTeamsInputHelp) {
+                        Alert(title: Text("Input Format:"), message: Text("<Team A name> <Team A registration date in DD/MM> <Team A group number>"), dismissButton: .default(Text("Okay")))
+                    }
+                })
+                
+                Section(content: {
+                    ZStack(alignment: .leading) {
+                        if matchInputText.isEmpty {
+                            HStack {
+                                Text("<Team A name> <Team B name> <Team A goals scored> <Team B goals scored>")
+                                    .font(.caption)
+                                    .foregroundColor(.secondary)
+                                Spacer()
+                            }
+                        }
+                        TextEditor(text: $matchInputText)
+                            .font(.caption)
+                            .foregroundColor(.primary)
+                    }
+                    
+                }, header: {
+                    HStack {
+                        Text("Enter Match Results here")
+                        Button {
+                            showMatchInputHelp = true
+                        } label: {
+                            Image(systemName: "info.circle")
+                        }
+                    }
+                    .alert(isPresented: $showMatchInputHelp) {
+                        Alert(title: Text("Input Format:"), message: Text("<Team A name> <Team B name> <Team A goals scored> <Team B goals scored>"), dismissButton: .default(Text("Okay")))
+                    }
+                })
+            }
+            .navigationTitle("Team Set-up")
+        }
     }
 }
 
