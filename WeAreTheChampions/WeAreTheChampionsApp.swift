21e48b05d7b1a10222cd0b8357fca712a04bdff7
Ameerul Basheer <ameerulbasheer@outlook.com>
2022-07-08 15:35:28 +0800

    Initial Commit

---
+++ WeAreTheChampions/WeAreTheChampionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeAreTheChampionsApp.swift
+//  WeAreTheChampions
+//
+//  Created by Ameerul Basheer on 8/7/22.
+//
+
+import SwiftUI
+
+@main
+struct WeAreTheChampionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
